8dd7ee42c2f66e414457ab9c967daeec81bc912d
segundoisammirpastorcerna-coder <segundoisammirpastorcerna@gmail.com>
2026-09-21 23:54:55 -0500

    Create citas.py
    Agregar la clase de CitaMedica

---
+++ citas.py
@@ -0,0 +1,13 @@
+class CitaMedica:
+    def __init__(self, codigo, paciente, fecha, especialidad, medico):
+        self.codigo = codigo
+        self.paciente = paciente
+        self.fecha = fecha
+        self.especialidad = especialidad
+        self.medico = medico
+    def mostrar_informacion(self):
+        print("Código:", self.codigo)
+        print("Paciente:", self.paciente)
+        print("Fecha:", self.fecha)
+        print("Especialidad:", self.especialidad)
+        print("Médico:", self.medico)
