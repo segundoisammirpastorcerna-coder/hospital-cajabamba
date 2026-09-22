005a9036ec49bb4814cb4980063dbeb33f59704d
segundoisammirpastorcerna-coder <segundoisammirpastorcerna@gmail.com>
2026-09-21 23:55:42 -0500

    Create funciones.py
    Agregar funciones de programación funcional

---
+++ funciones.py
@@ -0,0 +1,20 @@
+def filtrar_por_especialidad(citas, especialidad):
+    return list(filter(
+        lambda cita: cita.especialidad.lower() == especialidad.lower(),
+        citas
+    ))
+def filtrar_por_medico(citas, medico):
+    return list(filter(
+        lambda cita: cita.medico.lower() == medico.lower(),
+        citas
+    ))
+def transformar_fechas(citas):
+    return list(map(
+        lambda cita: cita.fecha,
+        citas
+    ))
+def ordenar_por_fecha(citas):
+    return sorted(
+        citas,
+        key=lambda cita: cita.fecha
+    )
