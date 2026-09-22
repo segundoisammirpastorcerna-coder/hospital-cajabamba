8665d9279055f9ed76846650ff8f87fb3fffd4ce
segundoisammirpastorcerna-coder <segundoisammirpastorcerna@gmail.com>
2026-09-21 23:54:06 -0500

    Create pacientes.py
    Agregar la clase del Paciente

---
+++ pacientes.py
@@ -0,0 +1,14 @@
+class Paciente:
+    def __init__(self, dni, nombres, apellidos, edad, telefono):
+        self.dni = dni
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.edad = edad
+        self.telefono = telefono
+    def mostrar_informacion(self):
+        print("DNI:", self.dni)
+        print("Nombres:", self.nombres)
+        print("Apellidos:", self.apellidos)
+        print("Edad:", self.edad)
+        print("Teléfono:", self.telefono)
+     
